23834292b6f86381306673d5cf07492e90e2aef3
pablobgldo <pablobgldo@gmail.com>
2024-10-10 11:22:44 +0100

    Completed Day 3 exercises

--- exercises/8b.sql
+++ exercises/8b.sql
@@ -2,3 +2,6 @@
 -- Could you delete Dua Lipa's albums, too?
 -- Pretty sure her ID was 6M2wZ9GZgrQXHCFfjv46we
 -- Don't worry about the tracks, I'll get someone else to do that. kthx!
+
+DELETE FROM albums
+WHERE artist_id = '6M2wZ9GZgrQXHCFfjv46we';
