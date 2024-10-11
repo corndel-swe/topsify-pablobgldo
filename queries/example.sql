18b37fc112b22e28236f1fa09c3b86d99c7f69fd
pablobgldo <pablobgldo@gmail.com>
2024-10-11 14:32:15 +0100

    Finished Day 3 Workshop

--- queries/example.sql
+++ queries/example.sql
@@ -5,3 +5,19 @@
 SELECT name, duration_ms
 FROM tracks
 LIMIT 5;
+
+SELECT id, name
+FROM tracks
+WHERE album_id = '552zi1M53PQAX5OH4FIdTx';
+
+SELECT users.username, playlists.name FROM playlists
+INNER JOIN users ON users.id = playlists.user_id;
+
+SELECT users.username, tracks.name
+FROM users
+INNER JOIN playlists ON users.id = playlists.user_id
+INNER JOIN tracks ON tracks.id = playlist_tracks.track_id
+INNER JOIN playlist_tracks ON playlists.id = playlist_tracks.playlist_id;
+
+SELECT * FROM playlist_tracks;
+
---
+++ queries/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO playlists (user_id, name) VALUES
+(1, 'Hot girl summer'),
+(1, 'Hip hop'),
+(2, 'Soundtracks');
+
+INSERT INTO playlist_tracks (playlist_id, track_id) VALUES
+(1, '1MpCaOeUWhox2Fgigbe1cL'),
+(1, '2mqTtvbKxH7SoEQ2oGAnsA'),
+(2, '3cf4iSSKd8ffTncbtKljXw'),
+(2, '4rmVZajAF7PkrCagGPHbqa'),
+(3, '1T41VK2fuFYbsXjIM7qKif'),
+(3, '3FrP4HVPTAZPDhDtClu5wX');
